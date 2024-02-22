e21f20931346f7052fe033cd149d50657ca30dac
siriuzz <elianmatosd03@gmail.com>
2024-02-21 23:15:12 -0400

    skills casi terminadas

---
+++ main.js
@@ -0,0 +1,29 @@
+// const links = [
+//     ['Home', '#home'],
+//     ['About me', '#about'],
+//     ['Skills', '#skills'],
+//     ['Projects', '#projects'],
+//     ['Contact', '#contact']
+// ];
+
+
+// const navBar = document.getElementById('navBar');
+
+// links.forEach(([title, url]) => {
+//     const link = document.createElement('a');
+//     link.href = url;
+//     link.classList = ['font-fredoka sm:text-2xl py-3'];
+//     link.textContent = title;
+//     navBar.appendChild(link);
+// });
+
+function scrolltoTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scrolling behavior
+    })
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+// });
