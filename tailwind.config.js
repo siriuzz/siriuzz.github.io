ca3b14887b7c4dd8c0654a22df6292ab90ce03f6
siriuzz <elianmatosd03@gmail.com>
2024-04-27 01:09:18 -0400

    pp and style added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", './index.html'],
   theme: {
+    extend: {
+      dropShadow: {
+        '2xl': '0px 0px 55px rgba(48, 122, 234, 0.55)',
+      }
+    },
     colors: {
       'blue': '#1b3a67',
       'light-blue': '#224A85',
